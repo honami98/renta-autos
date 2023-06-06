f5e36439a44ea478a90dce7fe98536a60d002aa0
Danileon0226 <122113337+Danileon0226@users.noreply.github.com>
2023-06-06 08:28:43 -0500

    Commit Mapeo Cars Firebase

--- src/Screens/Home/Home.jsx
+++ src/Screens/Home/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import Footer from "../../Components/Footer/Footer";
@@ -6,48 +6,76 @@ import { Link } from "react-router-dom";
 
 import Navbar from "../../Components/Navbar/Navbar";
 import "./Home.css";
+import db from "../../Firestore";
+import Swal from "sweetalert2";
+import { useNavigate } from "react-router-dom";
 
 function Home() {
   const [currentSlide, setCurrentSlide] = useState(0);
+  const [cars, setCars] = useState([]);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const fetchCars = async () => {
+      Swal.fire({
+        title: "Cargando...",
+        text: "Obteniendo datos",
+        icon: "info",
+        allowOutsideClick: false,
+        showCancelButton: false,
+        showConfirmButton: false,
+        onBeforeOpen: () => {
+          Swal.showLoading();
+        },
+      });
+
+      try {
+        const carsRef = db.collection("cars");
+        const snapshot = await carsRef.get();
+
+        const fetchedCars = snapshot.docs.map((doc) => {
+          const carData = doc.data();
+          return {
+            id: doc.id,
+            src: carData.imagen,
+            alt: carData.placa,
+            caption: carData.placa,
+            credit: `Disponible: ${carData.disponible ? "Sí" : "No"}`,
+          };
+        });
+
+        setCars(fetchedCars);
+        Swal.close();
+      } catch (error) {
+        Swal.fire({
+          title: "Error",
+          text: "No se ha podido obtener los datos. Revisa la conexión a internet.",
+          icon: "error",
+          confirmButtonText: "Aceptar",
+        });
+      }
+    };
+
+    fetchCars();
+  }, []);
 
   const handleSlideChange = (index) => {
     setCurrentSlide(index);
   };
 
-  const renderSlides = () => {
-    const slides = [
-      {
-        id: 1,
-        src: "https://newevolutiondesigns.com/images/freebies/4k-car-wallpaper-8.jpg",
-        alt: "Lamborghini Aventador SV Coupe",
-        caption: "Lamborghini Aventador SV Coupe",
-        credit: "Photo: Daniel",
-      },
-      {
-        id: 2,
-        src: "https://th.bing.com/th/id/R.fc7af22c00742be4f98b2bc8d8e0ddf4?rik=NC5uCzExMgVY3w&riu=http%3a%2f%2fyesofcorsa.com%2fwp-content%2fuploads%2f2018%2f07%2f4K-Cars-Best-Wallpaper.jpg&ehk=h84rIcqwJ9jlGR%2b41IdV0GrC%2flAShfnFE5IC2YsV%2b4k%3d&risl=&pid=ImgRaw&r=0",
-        alt: "Car 2",
-        caption: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
-        credit: "Photo: Jane Smith",
-      },
-      {
-        id: 3,
-        src: "https://cutewallpaper.org/21/wallpaper-jdm/My-List-Of-Jdm-Wallpaper-Pictures-For-Your-Phone-Enjoy-Jdm-.jpg",
-        alt: "Car 3",
-        caption: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
-        credit: "Photo: Alex Johnson",
-      },
-      // ...rest of the slides
-    ];
+  const handleRentCar = (ruta) => {
+    navigate(ruta);
+  };
 
-    return slides.map((slide, index) => (
-      <div key={slide.id} className="carousel__slide">
-        <img src={slide.src} alt={slide.alt} />
+  const renderSlides = () => {
+    return cars.map((car) => (
+      <div key={car.id} className="carousel__slide">
+        <img src={car.src} alt={car.alt} />
         <p className="legend">
-          {slide.caption}
+          {car.caption}
           <br />
           <br />
-          <span className="credit">{slide.credit}</span>
+          <span className="credit">{car.credit}</span>
         </p>
       </div>
     ));
@@ -69,6 +97,9 @@ function Home() {
             >
               {renderSlides()}
             </Carousel>
+            <button onClick={() => handleRentCar("/RentarAuto")}>
+              Rentar Auto
+            </button>
           </div>
         </section>
       </div>
